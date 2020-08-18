c2cf2a0f74b56fb91ae7caa31bef03823866b1e2
Jafarov Jafar <jafarovjafar@yahoo.com>
2020-08-18 11:45:54 +0700

    wip

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows.Forms;
 using System.Drawing;
+using System.IO;
 
 namespace ShafirScreenshoter
 {
@@ -7,7 +8,30 @@ namespace ShafirScreenshoter
     {
         static void Main(string[] args)
         {
+            CheckCfg();
+            GetVars();
             TakeScreenshot();
+        }
+
+        private static string path = "//192.168.44.32/Users2/Obmen/646/screens/";
+
+        private static void CheckCfg()
+        {
+            if (!File.Exists("ShafirScreenshoter.cfg"))
+            {
+                FileStream fileStream = File.Create("ShafirScreenShoter.cfg");
+                fileStream.Close();
+
+                StreamWriter streamWriter = new StreamWriter("ShafirScreenshoter.cfg");
+                streamWriter.WriteLine(path);
+                streamWriter.Close();
+            }
+        }
+
+        private static void GetVars()
+        {
+            StreamReader streamReader = new StreamReader("ShafirScreenshoter.cfg");
+            path = streamReader.ReadLine();
         }
 
         private static void TakeScreenshot()
@@ -15,7 +39,7 @@ namespace ShafirScreenshoter
             Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
             Graphics graphics = Graphics.FromImage(printscreen as Image);
             graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
-            printscreen.Save("1.jpeg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            printscreen.Save(path + "1.jpeg", System.Drawing.Imaging.ImageFormat.Jpeg);
         }
     }
 }

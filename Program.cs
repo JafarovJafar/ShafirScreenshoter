3dc600aad49f6e94539c27e9373f969764339f49
Jafarov Jafar <jafarovjafar@yahoo.com>
2020-08-18 10:31:20 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+namespace ShafirScreenshoter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TakeScreenshot();
+        }
+
+        private static void TakeScreenshot()
+        {
+            Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics graphics = Graphics.FromImage(printscreen as Image);
+            graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+            printscreen.Save("1.jpeg", System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+    }
+}
